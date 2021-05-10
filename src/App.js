3ba412f4d59a43925e99bafd3f7c53210cea9bff
inescss <ines.abdouli18@gmail.com>
2021-05-10 09:46:48 +0200

    react component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+
 import Address from './Component/Profile/Address';
 import FullName from './Component/Profile/FullName';
 import ProfilePhoto from './Component/Profile/ProfilePhoto';
